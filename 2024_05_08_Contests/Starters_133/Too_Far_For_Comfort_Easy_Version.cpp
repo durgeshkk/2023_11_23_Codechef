0ca28045d01dce540a7e0c6cc2ac1675b38dadc5
durgeshkk <durgeshkhandelwal2019@gmail.com>
2024-05-09 00:02:35 +0530

    Add: Starters-133 Solutions A-D

---
+++ 2024_05_08_Contests/Starters_133/Too_Far_For_Comfort_Easy_Version.cpp
@@ -0,0 +1,190 @@
+/*
+Once in a LifeTime,
+Will never let you Down!!
+*/
+#include <bits/stdc++.h>
+#include<iomanip>
+#include <deque>
+#include <bitset>
+#include <cstdint>
+//#include <ext/pb_ds/assoc_container.hpp> // Common file
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ll long long int
+#define all(a) a.begin(),a.end()
+#define enter(a) for(ll i=0;i<a.size();i++) cin>>a[i];
+#define forj(n) for (ll j = 0; j < n; j++)
+#define show(a) for(auto e: a) cout<<e<<" "; cout<<endl;
+using namespace std;
+ll mod = (ll)(1e9+7);
+
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+/*
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 10; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return mod_add(arr[0], 0, b);} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+bool revsort(ll a, ll b) {return a > b;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0)n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+*/
+
+// Fxn call(for ((b/a)%mod)) :
+// ll c = (b*power(a,mod-2(prime num in power of a),mod))%mod;
+// Also for formula like nCr..
+ll power(ll base,ll n,ll mod){
+    ll ans = 1;
+    while( n!= 0){
+        if(n%2){
+            n-=1;
+            ans = (ans*base)%mod;
+        }
+        else{
+            n /= 2;
+            base = (base*base)%mod;
+        }
+    }
+return ans;
+}
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll sieve[1000005];
+void createsieve(){
+    ll ntlen = 1000000;
+    sieve[0] = 0;
+    sieve[1] = 0;
+    for(ll i = 2;i<=ntlen;++i){
+        sieve[i] = 1;
+    }
+
+    for(ll i = 2;i*i<=ntlen;++i){
+        // Set all multiples to 0
+        if(sieve[i] == 1){
+
+            for(ll j = i*i;j<=ntlen;j+=i){
+                sieve[j] = 0;
+            }
+        }
+    }
+}
+
+
+vector<ll> adj[100004];
+
+void tree(){
+    ll n;
+    cin>>n;
+
+    ll u,v;
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+//  "A" : 65, "a" : 97  (-> |) (<- &(~))
+// YE DIL MAANGE MORE!!
+// array to store inverse of 1 to N
+const ll N = 5e5+5;
+ll factorialNumInverse[N + 1];
+ 
+// array to precompute inverse of 1! to N!
+ll naturalNumInverse[N + 1];
+ 
+// array to store factorial of first N numbers
+ll fact[N + 1];
+ 
+// Function to precompute inverse of numbers
+void InverseofNumber(ll p)
+{
+    naturalNumInverse[0] = naturalNumInverse[1] = 1;
+    for (int i = 2; i <= N; i++)
+        naturalNumInverse[i] = naturalNumInverse[p % i] * (p - p / i) % p;
+}
+// Function to precompute inverse of factorials
+void InverseofFactorial(ll p)
+{
+    factorialNumInverse[0] = factorialNumInverse[1] = 1;
+ 
+    // precompute inverse of natural numbers
+    for (int i = 2; i <= N; i++)
+        factorialNumInverse[i] = (naturalNumInverse[i] * factorialNumInverse[i - 1]) % p;
+}
+ 
+// Function to calculate factorial of 1 to N
+void factorial(ll p)
+{
+    fact[0] = 1;
+ 
+    // precompute factorials
+    for (int i = 1; i <= N; i++) {
+        fact[i] = (fact[i - 1] * i) % p;
+    }
+}
+ 
+// Function to return nCr % p in O(1) time
+ll Binomial(ll N, ll R, ll p)
+{
+    // n C r = n!*inverse(r!)*inverse((n-r)!)
+    ll ans = ((fact[N]*factorialNumInverse[R])%p * factorialNumInverse[N - R]) %p;
+    return ans;
+} 
+
+/*
+Lets say n = 8 and we have m = 10 and also we need ∣fi(x)−fi(y)∣≤1, for which 
+only possible pattern could be groups repeating one after other like 
+[1,2,3][1,2,3][Any 2 of (1,2,3)] and this groups can range from [1,min(N,M)].
+Also this groups can rearrange themselves in (group_members_cnt)!
+*/
+void solve(){
+    ll n,m;cin>>n>>m;
+    ll p = 998244353;
+    ll ans = 0;
+
+    ll i = 1;
+    while(i<=min(n,m)){
+        ll y = (n%i);
+        ll x = power(fact[i],n/i,p);
+        ll sa = mod_mul(Binomial(m,i,p),x,p);
+        ll sa2 = mod_mul(Binomial(i,y,p),fact[y],p);
+        sa = mod_mul(sa,sa2,p);
+        ans = mod_add(ans,sa,p);
+        ++i;
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll p = 998244353;
+    InverseofNumber(p);
+    InverseofFactorial(p);
+    factorial(p);
+    //setprecision(20);// also use precision with ans now
+    // use setprecision before ans
+    //precomputefact();
+    //createsieve();
+    //bfs();//call in solve()
+    //dfs(1,0);//call in solve()
+    
+    ll t=1;
+    cin >> t;
+    ll i = 1;
+    while (t--)
+    {
+        solve();
+        ++i;
+    }
+    //solve();
+    return 0;
+}
